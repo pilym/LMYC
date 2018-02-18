c16619b7abb154b0dfb5308688727e772213cb5c
Paul Li <paulli@shaw.ca>
2018-02-17 19:04:23 -0800

    Add user migrations

--- LmycWebSite/Controllers/AccountController.cs
+++ LmycWebSite/Controllers/AccountController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using LmycWebSite.Models;
+using LmycDataLib.Models;
 
 namespace LmycWebSite.Controllers
 {
---
+++ LmycWebSite/Migrations/IdentityMigrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace LmycWebSite.Migrations.IdentityMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LmycDataLib.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\IdentityMigrations";
+            ContextKey = "LmycDataLib.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(LmycDataLib.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
